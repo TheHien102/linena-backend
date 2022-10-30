c4d8fd041efbfe2d83f8464f2ebb9d205a8a96dc
TheHien102 <huynhthehien11102001@gmail.com>
2022-10-30 13:04:22 +0700

    Initial

---
+++ com-landingis-api/src/main/java/com/landingis/api/storage/model/TablePrefix.java
@@ -0,0 +1,9 @@
+package com.landingis.api.storage.model;
+
+public class TablePrefix {
+    public static final String PREFIX_TABLE="linena_";
+
+    private TablePrefix(){
+        //You cannot initial me
+    }
+}
