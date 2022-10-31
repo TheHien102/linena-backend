b3bd827aa350b49b4e030a1c33bfb3071eec821a
VoHuuGiau2901 <vohuugiau2901@gmail.com>
2022-10-31 11:18:59 +0700

    update for deploy

--- com-landingis-api/src/main/java/com/landingis/api/mapper/NewsMapper.java
+++ com-landingis-api/src/main/java/com/landingis/api/mapper/NewsMapper.java
@@ -9,7 +9,7 @@ import org.mapstruct.*;
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface NewsMapper {
 
 
